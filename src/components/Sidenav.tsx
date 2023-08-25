3d9bde536d4e4e37cbee2b83e3bc05a2838d3a97
José Caldas <jrcaldas@yahoo.com.br>
2023-08-25 15:01:08 -0300

    add Sidenav

--- src/components/Sidenav.tsx
+++ src/components/Sidenav.tsx
@@ -1,5 +1,55 @@
+import resumo from '../assets/icons/resumo.svg'
+import vendas from '../assets/icons/vendas.svg'
+import webhooks from '../assets/icons/webhooks.svg'
+import configuracoes from '../assets/icons/configuracoes.svg'
+import contato from '../assets/icons/contato.svg'
+import sair from '../assets/icons/sair.svg'
+import FintechSVG from './FintechSVG'
+
 const Sidenav = () => {
-  return <div>Sidenav</div>
+  return (
+    <nav className="sidenav box bg-3">
+      <FintechSVG title="Fintech Logo" />
+      <ul>
+        <li>
+          <span>
+            <img src={resumo} alt="Home Icon" />
+          </span>
+          <a href="">Resumo</a>
+        </li>
+        <li>
+          <span>
+            <img src={vendas} alt="Icon" />
+          </span>
+          <a href="">Vendas</a>
+        </li>
+        <li>
+          <span>
+            <img src={webhooks} alt="Icon" />
+          </span>
+          <a>Webhooks</a>
+        </li>
+        <li>
+          <span>
+            <img src={configuracoes} alt="Icon" />
+          </span>
+          <a>Configurações</a>
+        </li>
+        <li>
+          <span>
+            <img src={contato} alt="Icon" />
+          </span>
+          <a>Contato</a>
+        </li>
+        <li>
+          <span>
+            <img src={sair} alt="Icon" />
+          </span>
+          <a>Sair</a>
+        </li>
+      </ul>
+    </nav>
+  )
 }
 
 export default Sidenav
